3513e1f83bb2504e27b615bf2e85adf50f7df819
SangHwi-Back <sanghwiback@gmail.com>
2025-10-06 18:47:09 +0900

    Add button and animation start

--- SimpleNeatPomodoro/app/src/main/java/com/example/simpleneatpomodoro/MainActivity.kt
+++ SimpleNeatPomodoro/app/src/main/java/com/example/simpleneatpomodoro/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.simpleneatpomodoro
 
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        findViewById<Button>(R.id.start_button).setOnClickListener {
+            val progressView = findViewById<CircularProgressView>(R.id.circular_progress_view)
+
+            // 테스트를 위해 애니메이션으로 progress 값을 0에서 1로 5초 동안 변경
+            val animator = ValueAnimator.ofFloat(0f, 1f)
+            animator.duration = 5000 // 5초
+            animator.addUpdateListener { animation ->
+                val progressValue = animation.animatedValue as Float
+                // progress 값을 업데이트하면 커스텀 뷰가 자동으로 다시 그려짐
+                progressView.progress = progressValue
+            }
+            animator.start()
         }
     }
 }
