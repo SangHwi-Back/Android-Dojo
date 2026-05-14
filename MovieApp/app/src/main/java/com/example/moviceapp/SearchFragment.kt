be1e27cba36f8a708bc6767b7d3a76bde2fd629d
백상휘 <sanghwiback@gmail.com>
2026-05-14 17:22:58 +0900

    [MovieApp] Add some recycler view work

--- MovieApp/app/src/main/java/com/example/moviceapp/SearchFragment.kt
+++ MovieApp/app/src/main/java/com/example/moviceapp/SearchFragment.kt
@@ -5,55 +5,74 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.moviceapp.databinding.FragmentSearchBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+// https://jtm0609.tistory.com/261
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentSearchBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+    ): View {
+        binding = FragmentSearchBinding.inflate(inflater)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.recentSearchChipRecyclerView
+    }
+
+    class SearchRecentChipViewAdapter: RecyclerView.Adapter<SearchRecentChipViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): SearchRecentChipViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val listItem = layoutInflater.inflate(
+                R.layout.search_recent_list_item, parent, false)
+            return SearchRecentChipViewHolder(listItem)
+        }
+
+        override fun onBindViewHolder(
+            holder: SearchRecentChipViewHolder,
+            position: Int
+        ) {
+            holder.nameText.text = "Hello!"
+        }
+
+        override fun getItemCount(): Int = 1
+    }
+
+    class SearchRecentChipViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameText: TextView = itemView.findViewById(R.id.name_text_view)
+    }
+
+    class TrendingNowViewAdapter : RecyclerView.Adapter<TrendingNowViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): TrendingNowViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val listItem = layoutInflater.inflate(
+                R.layout.trending_now_list_item, parent, false)
+            return TrendingNowViewHolder(listItem)
+        }
+
+        override fun onBindViewHolder(
+            holder: TrendingNowViewHolder,
+            position: Int
+        ) {
+            holder.nameText.text = "Hello!"
+        }
+
+        override fun getItemCount(): Int = 1
+    }
+
+    class TrendingNowViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val nameText: TextView = itemView.findViewById(R.id.name_text_view)
     }
 }
