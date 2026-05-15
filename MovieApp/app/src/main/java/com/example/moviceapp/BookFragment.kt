0fe3c286acb96ec4f24b508d6ef2fece947e8651
백상휘 <sanghwiback@gmail.com>
2026-05-15 15:08:40 +0900

    [MovieApp] Add Booking screen grid

--- MovieApp/app/src/main/java/com/example/moviceapp/BookFragment.kt
+++ MovieApp/app/src/main/java/com/example/moviceapp/BookFragment.kt
@@ -1,59 +1,64 @@
 package com.example.moviceapp
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.moviceapp.SearchFragment.BrowseAllViewHolder
+import com.example.moviceapp.databinding.FragmentBookBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [BookFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class BookFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding: FragmentBookBinding
+    private val movies = MoviesMock.all
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_book, container, false)
+    ): View {
+        binding = FragmentBookBinding.inflate(inflater)
+        return binding.root
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BookFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BookFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.root.width
+        binding.bookRecyclerGridView.apply {
+            adapter = MovieViewAdapter(movies)
+        }
+    }
+    class MovieViewAdapter(
+        val movies: List<Movie>
+    ): RecyclerView.Adapter<MovieViewHolder>() {
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): MovieViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val listItem = layoutInflater.inflate(
+                R.layout.trending_now_list_item, parent, false)
+            listItem.layoutParams.width = (DisplayMetrics().widthPixels - 16) / 3
+            return MovieViewHolder(listItem)
+        }
+        override fun onBindViewHolder(
+            holder: MovieViewHolder,
+            position: Int
+        ) = holder.setMovie(movies[position])
+        override fun getItemCount(): Int = movies.size
+    }
+    class MovieViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun setMovie(movie: Movie) {
+            itemView.findViewById<ImageView>(R.id.movie_image_view)
+                .load(movie.posterRes ?: R.drawable.ic_launcher_background)
+            itemView.findViewById<TextView>(R.id.name_text_view)
+                .text = movie.title
+            itemView.findViewById<TextView>(R.id.point_text_view)
+                .text = movie.rating.toString()
+        }
     }
 }
