2c21aff029fb906a9f84fca00733695f1f697b4a
백상휘 <sanghwiback@gmail.com>
2026-05-21 17:33:06 +0900

    [MovieApp] Add Book Theater fragment components

--- MovieApp/app/src/main/java/com/example/moviceapp/BookTheaterFragment.kt
+++ MovieApp/app/src/main/java/com/example/moviceapp/BookTheaterFragment.kt
@@ -1,16 +1,76 @@
 package com.example.moviceapp
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.moviceapp.databinding.FragmentBookTheaterBinding
+import com.example.moviceapp.databinding.ItemBookTheaterSelectTheaterBinding
 
 class BookTheaterFragment : Fragment() {
+    val args: BookTheaterFragmentArgs by navArgs()
+    val adapter: RecyclerViewAdapter = RecyclerViewAdapter()
+    lateinit var binding: FragmentBookTheaterBinding
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_book_theater, container, false)
+    ): View {
+        binding = FragmentBookTheaterBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.movieImageView.load(args.movie.posterRes ?: R.drawable.ic_launcher_background)
+        binding.movieNameTextView.text = args.movie.title
+        binding.movieRuntimeTextView.text = args.movie.duration
+        binding.moviePointTextView.text = args.movie.rating.toString()
+
+        binding.theaterRecyclerView.layoutManager = LinearLayoutManager(
+            requireActivity(),
+            LinearLayoutManager.VERTICAL,
+            false)
+        binding.theaterRecyclerView.adapter = adapter
+        adapter.setItems(TheatersMock.list)
+    }
+
+    class RecyclerViewAdapter: RecyclerView.Adapter<TheaterViewHolder>() {
+        private var items = mutableListOf<Theater>()
+        override fun onCreateViewHolder(
+            parent: ViewGroup,
+            viewType: Int
+        ): TheaterViewHolder {
+            val inflater = LayoutInflater.from(parent.context)
+            return TheaterViewHolder(ItemBookTheaterSelectTheaterBinding.inflate(inflater))
+        }
+        override fun onBindViewHolder(
+            holder: TheaterViewHolder,
+            position: Int
+        ) {
+            holder.bind(items[position])
+        }
+        override fun getItemCount(): Int = items.size
+        fun setItems(items: List<Theater>) {
+            if (this.items.size < items.size && this.items.isNotEmpty())
+                notifyItemRangeRemoved(this.items.size-1, items.size-1)
+            this.items = items.toMutableList()
+            notifyItemRangeChanged(0, this.items.size-1)
+        }
+    }
+    class TheaterViewHolder(
+        val binding: ItemBookTheaterSelectTheaterBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(theater: Theater) {
+            binding.nameTextView.text = theater.name
+            binding.addressTextView.text = theater.address
+            binding.distanceTextView.text = theater.distanceKm.toString()
+        }
     }
 }
