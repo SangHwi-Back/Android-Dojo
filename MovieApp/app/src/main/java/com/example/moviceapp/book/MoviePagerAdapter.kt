e60bac1326859c4d00d13741e81e714f238b9fc6
백상휘 <sanghwiback@gmail.com>
2026-07-02 11:07:15 +0900

    [MovieApp] CRITICAL ALERT
    - token, hash leaked.
    - remove token & hash from code
    - remove tokens for access
    - remove runner that used

--- MovieApp/app/src/main/java/com/example/moviceapp/book/MoviePagerAdapter.kt
+++ MovieApp/app/src/main/java/com/example/moviceapp/book/MoviePagerAdapter.kt
@@ -36,11 +36,3 @@ class MoviePagerAdapter(
         }
     }
 }
-
-/**
- *  4121* curl -o actions-runner-osx-x64-2.335.1.tar.gz -L https://github.com/actions/runner/releases/download/v2.335.1/actions-runner-osx-x64-2.335.1.tar.gz
- *  4122* echo "b2fe57b2ae5b0bc1605f9fc0723c07eedf06167321d3478ce0440f15e5b0a010  actions-runner-osx-x64-2.335.1.tar.gz" | shasum -a 256 -c
- *  4123* tar xzf ./actions-runner-osx-x64-2.335.1.tar.gz
- *  4124* ./config.sh --url https://github.com/SangHwi-Back/Android-Dojo --token APXAQSB3YPCBD5ZJ2OPKJULKIUT7E
- *  4125* ./run.sh
- */
