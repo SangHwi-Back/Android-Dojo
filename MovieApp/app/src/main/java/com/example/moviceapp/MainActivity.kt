f519fdc43043c06224ef198f42f50c485aef4b97
SangHwi-Back <sanghwiback@gmail.com>
2026-07-14 21:27:18 +0900

    [MovieApp] Set AppBarLayout position and attribute

--- MovieApp/app/src/main/java/com/example/moviceapp/MainActivity.kt
+++ MovieApp/app/src/main/java/com/example/moviceapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.Menu
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.example.moviceapp.databinding.ActivityMainBinding
@@ -19,18 +21,35 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // WindowInsets 처리 - 시스템 바와 겹치지 않도록 패딩 추가
+        ViewCompat.setOnApplyWindowInsetsListener(binding.mainRoot) { view, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            // AppBarLayout 상단에 패딩 추가
+            binding.appBarLayout.setPadding(
+                insets.left,
+                insets.top,
+                insets.right,
+                0
+            )
+
+            // BottomNavigationView 하단에 패딩 추가
+            binding.navigationView.setPadding(
+                insets.left,
+                0,
+                insets.right,
+                insets.bottom
+            )
+
+            WindowInsetsCompat.CONSUMED
+        }
+
         setSupportActionBar(binding.topAppBar)
 
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.main_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         binding.navigationView.setupWithNavController(navController)
-
-//        navController.addOnDestinationChangedListener { _, destination, _ ->
-//            when (destination.id) {
-//
-//            }
-//        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
