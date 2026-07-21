4be5c70358e41ab41469bb6aff757803b1466afd
백상휘 <sanghwiback@gmail.com>
2026-07-21 09:50:45 +0900

    [Database] Add Select and more update functions

--- Kotlin-Database/Kotlin-Database/src/main/kotlin/Main.kt
+++ Kotlin-Database/Kotlin-Database/src/main/kotlin/Main.kt
@@ -36,13 +36,50 @@ data class Table(
 ) {
     override fun toString(): String =
         "📦 Table: {$name}, Columns: " + columns.joinToString(", ") { "${it.name}[${it.dataType}]" } + "]"
+    fun selectRows(
+        columns: List<Column>? = null,
+        where: List<Where> = listOf()
+    ): List<Row> {
+        fun selectAll(): List<Row> = rows.filter {
+            for (condition in where) {
+                for (record in it.records) {
+                    if (condition.column == record.column) {
+                        return@filter condition.data == record.data
+                    }
+                }
+            }
+            return@filter false
+        }
+        fun selectFrom(columns: List<Column>): List<Row> {
+            val result = mutableListOf<Row>()
+            outerLoop@ for (row in rows) {
+                var row = row
+                // Where 검증
+                for (condition in where) {
+                    for (record in row.records) {
+                        if (condition.column != record.column) {
+                            continue@outerLoop
+                        }
+                    }
+                }
+                // 원하는 컬럼만 추출
+                row = Row(records = row.records.filter { columns.contains(it.column) }.toMutableList())
+                result.add(row)
+            }
+            return result
+        }
+        return if (columns != null)
+            selectFrom(columns)
+        else
+            selectAll()
+    }
     @Throws(IllegalArgumentException::class)
     fun insertRow(data: List<String>) {
         if (columns.size != data.size)
             throw IllegalArgumentException("Columns count don't match")
         rows.add(Row(records = columns.mapIndexed { index, column ->
             Record(
-                column.name,
+                column,
                 data[index],
                 dataType = column.dataType
             )
@@ -50,7 +87,7 @@ data class Table(
     }
     @Throws(IllegalArgumentException::class)
     fun insertRecords(data: List<Record>) {
-        if (columns.map { it.name.lowercase() }.sorted() != data.map { it.column.lowercase() }.sorted())
+        if (columns.map { it.name.lowercase() }.sorted() != data.map { it.column.name.lowercase() }.sorted())
             throw IllegalArgumentException("Columns not equals")
         rows.add(Row(records = data.toMutableList()))
     }
@@ -59,10 +96,26 @@ data class Table(
         val rowIndex = rows.getRowIndex(key)
         if (rowIndex == null || rowIndex < 0) return
 
-        val columnIndex = rows[rowIndex].records.getColumnIndex(column)
-        if (columnIndex == null || columnIndex < 0) return
+        val columnIndex = rows[rowIndex].records.map { it.column.name }.indexOf(column)
+        if (columnIndex < 0) return
 
         rows[rowIndex].records[columnIndex].data = data
+    }
+    @Throws(NoSuchElementException::class)
+    fun updateRecord(row: Row) {
+        val rowIndex = rows.indexOf(row)
+        if (rowIndex < 0) return
+        rows[rowIndex] = row
+    }
+    @Throws(NoSuchElementException::class)
+    fun updateRecord(key: String, record: Record) {
+        val rowIndex = rows.getRowIndex(key)
+        if (rowIndex == null || rowIndex < 0) return
+
+        val columnIndex = rows[rowIndex].records.getColumnIndex(record.column)
+        if (columnIndex == null || columnIndex < 0) return
+
+        rows[rowIndex].records[columnIndex] = record
     }
     fun deleteRow(key: String) {
         val rowIndex = rows.getRowIndex(key)
@@ -72,13 +125,13 @@ data class Table(
 }
 fun MutableList<Row>.getRowIndex(key: String): Int? {
     for ((index, row) in this.withIndex()) {
-        val record = row.records.firstOrNull { it.column == "key" }
+        val record = row.records.firstOrNull { it.column.name == "key" }
         if (record != null && record.data == key)
             return index
     }
     return null
 }
-fun MutableList<Record>.getColumnIndex(key: String): Int? {
+fun MutableList<Record>.getColumnIndex(key: Column): Int? {
     for ((index, record) in this.withIndex()) {
         if (record.column == key)
             return index
@@ -89,23 +142,24 @@ data class Column(
     val name: String,
     val dataType: DBDataType,
 )
-
 data class Row(
     val records: MutableList<Record>
 ) {
     override fun toString(): String =
         records.joinToString(", ")
 }
-
 data class Record(
-    val column: String,
+    val column: Column,
     var data: String,
     val dataType: DBDataType,
 ) {
     override fun toString(): String =
         "[{$dataType} $column]: $data"
 }
-
+data class Where(
+    val column: Column,
+    val data: String,
+)
 enum class DBDataType {
     NUMBER, VARCHAR, DATE, EMAIL;
 
