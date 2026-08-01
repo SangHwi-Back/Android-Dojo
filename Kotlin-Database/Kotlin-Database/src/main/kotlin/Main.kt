c070d9e2c0532ce383b08af57fe44aeefdd0af3c
SangHwi-Back <sanghwiback@gmail.com>
2026-08-01 18:36:35 +0900

    [Database] Add more transaction

--- Kotlin-Database/Kotlin-Database/src/main/kotlin/Main.kt
+++ Kotlin-Database/Kotlin-Database/src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonPrimitive
+import org.example.TableColumn
 import java.io.File
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -79,6 +80,11 @@ suspend fun main() {
                 TableColumn("email", TableColumnType.Varchar) set "bigfoot@pentagon.com"
             }),
             Operation.Select(table.name, table.tableColumns),
+            Operation.Insert(table.name, table.newRow {
+                TableColumn("name", TableColumnType.Varchar) set "TestUser"
+                TableColumn("birth", TableColumnType.DateTime) set "2000-01-01"
+                TableColumn("email", TableColumnType.Varchar) set "testuser@example.com"
+            })
         )).let {
             database.insertTransaction(it)
         }
@@ -93,6 +99,19 @@ suspend fun main() {
         )).let {
             database.insertTransaction(it)
         }
+
+        Transaction(table.hashCode().toString(), listOf(
+            Operation.Update(
+                table.name,
+                listOf(
+                    TableRecord(TableColumn("name", TableColumnType.Varchar), "UpdatedUser"),
+                    TableRecord(TableColumn("birth", TableColumnType.DateTime), "2001-01-01"),
+                    TableRecord(TableColumn("email", TableColumnType.Varchar), "updateduser@example.com")
+                ),
+                listOf()
+            ),
+            Operation.Delete(table.name, ConditionDelete.ID(0))
+        ))
     }
 
     delay(1000L.milliseconds)
@@ -110,8 +129,11 @@ fun MutableList<TableRow>.getRowIndex(key: String): Int? {
         }
     return null
 }
+
 fun MutableList<out TableRecord<Any>>.columnIndex(column: TableColumn<Any>): Int =
     map { it.tableColumn }.indexOf(column)
+
 fun TableRow.getRecord(column: TableColumn<Any>): TableRecord<Any>? =
     tableRecords.columnIndex(column).let {
-        return if (it >= 0) tableRecords[it] else null }
+        return if (it >= 0) tableRecords[it] else null
+    }
