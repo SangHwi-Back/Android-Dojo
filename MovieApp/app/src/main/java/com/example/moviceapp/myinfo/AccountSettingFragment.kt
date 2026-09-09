0fce7f7b9b2b3b17b6fa5d29da620ad20e93548c
sanghwiback <sanghwiback@gmail.com>
2026-09-09 23:40:33 +0900

    [MovieApp] Edit layout fragment_account_setting

--- MovieApp/app/src/main/java/com/example/moviceapp/myinfo/AccountSettingFragment.kt
+++ MovieApp/app/src/main/java/com/example/moviceapp/myinfo/AccountSettingFragment.kt
@@ -1,6 +1,18 @@
 package com.example.moviceapp.myinfo
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.moviceapp.databinding.FragmentAccountSettingBinding
 
 class AccountSettingFragment : Fragment() {
+    private var _binding: FragmentAccountSettingBinding? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        val binding = FragmentAccountSettingBinding.inflate(inflater)
+        this._binding = binding
+        return binding.root
+    }
 }
