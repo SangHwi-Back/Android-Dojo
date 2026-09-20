be5bfb406bd4049f4fde155268d7b6b2a21ab8b4
sanghwiback <sanghwiback@gmail.com>
2026-09-20 09:54:32 +0900

    [MovieApp] Resolve crash on SignInFragment

--- MovieApp/app/src/main/java/com/example/moviceapp/repo/MovieAppUserService.kt
+++ MovieApp/app/src/main/java/com/example/moviceapp/repo/MovieAppUserService.kt
@@ -1,6 +1,7 @@
 package com.example.moviceapp.repo
 
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.PATCH
@@ -12,5 +13,5 @@ interface MovieAppUserService {
     @GET("/api/users/me")
     fun getUser(@Header("Authorization") token: String?): Call<UserEntity>
     @PATCH("/api/users/me")
-    fun updateUser(user: UserEntity): Call<UserEntity>
+    fun updateUser(@Body user: UserEntity): Call<UserEntity>
 }
